981b862ee2340a5fc72a1050caafefdf49768077
userom11 <126608941+userom11@users.noreply.github.com>
2023-09-16 17:23:23 +0200

    Commit for @TheAnonymousCrusher

--- js/card.js
+++ js/card.js
@@ -1,10 +1,19 @@
+
 document.querySelectorAll('.card').forEach(card => {
     let title = card.querySelector('.card-title');
+    let description = card.querySelector('.card-description');
+
     title.style.whiteSpace = 'nowrap';
     title.style.overflow = 'hidden';
     title.style.textOverflow = 'ellipsis';
     title.style.transition = 'all 1s'; // Set transition for 1 sec fade-in
   
+    description.style.whiteSpace = 'nowrap';
+    description.style.overflow = 'hidden';
+    description.style.textOverflow = 'ellipsis';
+    description.style.transition = 'all 1s'; // Set transition for 1 sec fade-in
+
+
     card.addEventListener('click', () => {
       if (title.style.whiteSpace !== 'normal') {
         title.style.whiteSpace = 'normal';
@@ -15,8 +24,31 @@ document.querySelectorAll('.card').forEach(card => {
         title.style.overflow = 'hidden';
         title.style.textOverflow = 'ellipsis';
       }
+
+
+
+      if (description.style.whiteSpace !== 'normal') {
+        description.style.whiteSpace = 'normal';
+        description.style.overflow = 'visible';
+        description.style.textOverflow = 'clip';
+      } else{
+        description.style.whiteSpace = 'nowrap';
+        description.style.overflow = 'hidden';
+        description.style.textOverflow = 'ellipsis';
+      }
     });
   });
+
+
+
+
+
+  // When the modal is shown, disable scrolling on the body element
+document.body.style.overflow = 'hidden';
+
+// When the modal is hidden, enable scrolling on the body element
+document.body.style.overflow = '';
+
 
 
 
