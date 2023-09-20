d9445c19b8a25e5c4acc4fdb03872694d7960c5e
Crusher <129316307+TheAnonymousCrusher@users.noreply.github.com>
2023-09-20 21:53:31 +0200

    Added "Stagger" Effect

---
+++ js/detect-resolution.js
@@ -0,0 +1,30 @@
+// Detect screen resolution
+let screenHeight = window.screen.availHeight;
+let screenWidth = window.screen.availWidth;
+
+// Create an object with the data
+let screenData = {
+  height: screenHeight,
+  width: screenWidth
+};
+
+// Convert the data to JSON
+let jsonData = JSON.stringify(screenData);
+
+// Send the data to the server
+fetch('http://127.0.0.1:5500', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: jsonData
+}).then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+}).then(data => {
+  console.log('Success:', data);
+}).catch((error) => {
+  console.error('Error:', error);
+});
