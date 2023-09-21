37be97e337032fb2bb7102681a80ccf1329116c3
userom11 <126608941+userom11@users.noreply.github.com>
2023-09-21 23:11:17 +0200

    Update main.rs

--- backend2/src/main.rs
+++ backend2/src/main.rs
@@ -6,14 +6,14 @@ use actix_web::{web, App, HttpResponse, HttpServer};
 async fn main() {
 
 //JS hosting
-let _ = HttpServer::new(||{
+let JSs = HttpServer::new(||{
     App::new().route("/main.js", web::get().to(HttpResponse::Ok))
 })
          .workers(3)
          .bind(("127.0.0.1", 8080));
 
 //HTM & CSS hosting
-let _ = HttpServer::new(|| { 
+let HTMCSSs = HttpServer::new(|| { 
     App::new()
         .route("/home.htm", web::get().to(HttpResponse::Ok))
         .route("/main.css", web::get().to(HttpResponse::Ok))
@@ -21,4 +21,15 @@ let _ = HttpServer::new(|| {
               .bind(("127.0.0.1", 1010));
 
  }
+
+//Content hosting
+
+let content = HttpServer::new(||{
+    App::new()
+    .route("/content", web::get().top(HttpResponse::Ok))
+
+//content folder will have imgs and text in order, img->text->img->text....
+//will be sent in that order
+// line 1 of txt is title
+// the rest is the actual content
 
