5e188dc19511f79d391dc00f19279a29ad093c28
userom11 <126608941+userom11@users.noreply.github.com>
2023-09-26 20:44:58 +0200

    Update GPT-API.py

--- GPT-API.py
+++ GPT-API.py
@@ -1,12 +1,11 @@
-#GPT
+# GPT
 import openai
 import requests
 import json
 
-#GPT part
-#x & xx are going to be asigned a string every interval, this strings will be some interesting sci\philosophy\news\art question prompted to gpt. there are 2 to make this proccess faster.
-#x =
-#xx =
+
+# x str 'content 3 sentences from body'
+# x =
 openai.api_key = "api key"
 
 completion = openai.ChatCompletion.create(
@@ -16,7 +15,5 @@ completion = openai.ChatCompletion.create(
         {"role": "user", "content": "What is life?"}
         ]
 )
-#system gives context about user to assistant
-#user is the prompt
-#assistant is the gpt model
-#content shows the content of either system or user as shown above
+# system gives context about user to assistant
+# user is the prompt
